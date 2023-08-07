b6d1905647e18fe54f1c7e5bd58fc26baf43bc96
yeomsungpil <present7256@gmail.com>
2023-08-07 12:26:57 +0900

    git Ignore 방법

--- Networking_week4/ViewController.swift
+++ Networking_week4/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        callRequest()
     }
 
+    func callRequest() {
+        
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20031111"
+        
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
