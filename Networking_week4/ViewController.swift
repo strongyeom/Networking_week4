f0a3a97bd4657c64412848db9b84151188450aec
yeomsungpil <present7256@gmail.com>
2023-08-08 08:59:51 +0900

    네트워크 SwiftyJSON + Alamofire 활용 기본편 1

--- Networking_week4/ViewController.swift
+++ Networking_week4/ViewController.swift
@@ -20,11 +20,21 @@ class ViewController: UIViewController {
         
         let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(APIKey.boxOfficeKey)&targetDt=20031111"
         
+        // SwiftyJSON을 사용해서 Alamofire와 같이 활용해서 쉽게 데이터 통신을 할 수 있음
+        // method: .get은 데이터를 읽어오는것
+        // validate: 응답의 상태를 알려준다.
         AF.request(url, method: .get).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
                 print("JSON: \(json)")
+                
+                // JSON에서 원하는 것에 도달하려면 차근차근(단계별로) 타고 내려가야함
+                let name = json["boxOfficeResult"]["dailyBoxOfficeList"][0]["movieNm"].stringValue
+                print("name", name)
+                
+                
+                
             case .failure(let error):
                 print(error)
             }
