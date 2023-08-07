e0cc3610fed7bbf6c9cde9cf2141ca17df4fa7ab
yeomsungpil <present7256@gmail.com>
2023-08-07 12:34:22 +0900

    gitIgnore를 통한 키 숨기는 방법

--- Networking_week4/ViewController.swift
+++ Networking_week4/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
 
     func callRequest() {
         
-        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20031111"
+        let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(APIKey.boxOfficeKey)&targetDt=20031111"
         
         AF.request(url, method: .get).validate().responseJSON { response in
             switch response.result {
